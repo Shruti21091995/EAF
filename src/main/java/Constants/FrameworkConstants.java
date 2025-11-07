e6ef5585cae5b91f10714daeb8760ff48d206a27
Shruti199521 <patilshruti259@gmail.com>
2025-11-07 16:43:36 +0530

    Updated Project

--- src/main/java/Constants/FrameworkConstants.java
+++ src/main/java/Constants/FrameworkConstants.java
@@ -6,4 +6,6 @@ public class FrameworkConstants
   public static final String EXCEL_PATH="/EcommerceAutomation/src/test/resource/TestData/Users.xlsx";
   public static final int Explicit_Wait=10;
   public static final String ProductName="Camera";
+  public static final String ExcelSheetPath="C:\\Users\\shrut\\eclipse-workspace shruti\\EcommerceAutomation\\src\\test\\resources\\TestData\\LoginData.xlsx";
+  public static final String ExcelSheet="Sheet1";
 }
--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -3,6 +3,7 @@ package Base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import com.ecommerceAutomation.demowebshop.factory.WebDriverFactory;
 
@@ -13,14 +14,25 @@ public class BaseTest
 	public WebDriver driver;
 	public PropertyUtils proper;
 	
+	@Parameters   
 	@BeforeMethod
-	public void setup()
+	public void setup(String BrowserFromXml)
 	{
 		//Initialize PropertyUtils with config file path
 		proper = new PropertyUtils("src/test/resources/Config/Config.properties");
 		
 		//fetching the data from config.properties file
 		String browser = proper.GetPropery("browser");
+		if(browser==null || browser.isEmpty()) //Implementation of cross broswer testing in testng
+		{
+			browser=BrowserFromXml;
+		}
+		else
+		{
+			browser=proper.GetPropery(browser);
+		}
+		
+		
 		String BaseUrl = proper.GetPropery("url");
 		
 		//now invoke the webdriver based on the browser type and call the createdriver method of class WebDriverFactory
